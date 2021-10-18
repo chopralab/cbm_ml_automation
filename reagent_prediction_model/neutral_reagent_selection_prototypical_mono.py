052b8de8beef7e934dd6b55e1d3e2e2a37534838
gwijewar <gwijewar@purdue.edu>
2021-10-18 11:36:36 -0400

    ranking_nr

--- reagent_prediction_model/neutral_reagent_selection_prototypical_mono.py
+++ reagent_prediction_model/neutral_reagent_selection_prototypical_mono.py
@@ -23,9 +23,9 @@ from __future__ import print_function
 ## C6H15N1O0, rdbe = 0
 ## C6H8N1O0, rdbe = 4
 
-elemental_composition = 'C6H8N1O0' #'C4H7O2N1' #sys.argv[1]
+elemental_composition = 'C12H11O1S1' #'C4H7O2N1' #sys.argv[1]
 #elemental_composition = input('Input elemental composition (eg: C6H8N2O0; include Oxygen as zero if not present): ') # user = 'foobar12345'
-rdbe = 4 # int(input('RDBE: '))
+rdbe = 9 # int(input('RDBE: '))
 
 print('Elemental Composition: ',elemental_composition), print('RDBE: ', rdbe)
 print('\n')
@@ -65,63 +65,84 @@ def find_elements(string):
 def find_reagents_expert(lst_of_lst):
     elements = lst_of_lst[0]
     numbers = lst_of_lst[1]
+    reagents_expert = []
     
     reagents = ['TMB', 'TDMAB','MOP']
 
     if ('S' in elements and 'O' in elements and numbers[elements.index('O')] == 2) or ('S' in elements and 'O' in elements and numbers[elements.index('O')] == 2) or ('O' in elements and 'N' in elements and rdbe >= 0.5 and numbers[elements.index('O')] == 1 and numbers[elements.index('N')] == 1) or ('O' in elements and rdbe >= 0.5 and numbers[elements.index('O')] == 1) or  ('O' in elements and numbers[elements.index('O')] == 2):
         reagent = reagents[0]
-        print('\n',reagent, '\n')
+        reagents_expert.append(reagent)
+#        print('\n',reagent, '\n')
 
     if ('S' in elements and 'O' in elements and numbers[elements.index('O')] == 1 and rdbe >= 0.5) or ('O' in elements and numbers[elements.index('O')] == 1 and 'N' in elements and numbers[elements.index('N')] >= 0.5 and rdbe >= 0.5):
         reagent = reagents[1]
-        print('\n',reagent, '\n')
+        reagents_expert.append(reagent)
+#        print('\n',reagent, '\n')
     
 
     if ('S' in elements and 'O' in elements and numbers[elements.index('O')] == 1 and rdbe >= 0.5) or ('O' in elements and numbers[elements.index('O')] == 1 and 'N' in elements and numbers[elements.index('N')] >= 0.5) or ('N' in elements and numbers[elements.index('N')] == 2):
         reagent = reagents[2]
-        print('\n',reagent, '\n')
+        reagents_expert.append(reagent)
+#        print('\n',reagent, '\n')
+    
+    return reagents_expert
         
-#    else:
-#        print('No expert-based predictions', '\n')
-  
 
 def find_reagents_ml_based(lst_of_lst):
     elements = lst_of_lst[0]
     numbers = lst_of_lst[1]
-    
-#    print(elements)
-#    print(numbers)
-    
+    reagents_ml = []
+
     reagents = ['TMB', 'TDMAB','MOP']
   
     if (('S' in elements and numbers[elements.index('S')] == 1) and ('O' in elements and numbers[elements.index('O')] >= 1) and rdbe >= 1.5) or (('N' in elements and numbers[elements.index('N')] == 1) and ('O' in elements and numbers[elements.index('O')] == 1) and rdbe >= 0 and rdbe < 4) or (('O' in elements and numbers[elements.index('O')] >= 1) and rdbe >= 2 and rdbe < 4):
         reagent = reagents[0]
-        print('\n',reagent, '\n')  
+        reagents_ml.append(reagent)
+#        print('\n',reagent, '\n')  
         
     if ('O' in elements and numbers[elements.index('O')] == 1 and rdbe < 0.5) or (('O' in elements and numbers[elements.index('O')] == 1 ) and rdbe >= 0.5 and ('N' in elements and numbers[elements.index('N')] == 0)) or (('O' in elements and numbers[elements.index('O')] == 1) and ('N' in elements and numbers[elements.index('N')] == 1)) or ('N' in elements and numbers[elements.index('N')] == 0):
         reagent = reagents[1]
-        print('\n',reagent, '\n') 
+        reagents_ml.append(reagent)
+#        print('\n',reagent, '\n') 
     
     if ('S' in elements and numbers[elements.index('S')] == 1 and rdbe >= 0.5 and 'O' in elements and numbers[elements.index('O')] < 2) or ('N' in elements and numbers[elements.index('N')] == 1 and rdbe >= 0.5 and 'O' in elements and numbers[elements.index('O')] == 1) or ('S' in elements and numbers[elements.index('S')] == 1 and  rdbe == 1) or ('O' in elements and numbers[elements.index('O')] == 1 and 'N' in elements and numbers[elements.index('N')] == 1):
         reagent = reagents[2]
-        print('\n',reagent, '\n')
+        reagents_ml.append(reagent)
+#        print('\n',reagent, '\n')
         
-    
-#    else:
-#        print('No ML-based predictions')
+    return reagents_ml
 
 
 elements_list_of_list = find_elements(elemental_composition)
 
-print('expert-based neutral reagent predictions:')
-find_reagents_expert(elements_list_of_list)
+#print('expert-based neutral reagent predictions:')
+#find_reagents_expert(elements_list_of_list)
+#
+#print('\n')
+#print('\n')
+#
+#print( 'ML-based neutral reagent predictions:') 
+#find_reagents_ml_based(elements_list_of_list)
 
-print('\n')
-print('\n')
+expert = find_reagents_expert(elements_list_of_list)
+ml = find_reagents_ml_based(elements_list_of_list)
 
-print( 'ML-based neutral reagent predictions:') 
-find_reagents_ml_based(elements_list_of_list)
+#print(expert)
+#print(ml)
 
+list1_as_set = set(expert)
+
+intersection = list1_as_set.intersection(ml)
+intersection_as_list = list(intersection)
+#print(intersection_as_list)
+
+sym_dif = list1_as_set.symmetric_difference(ml)
+sym_diff_as_list = list(sym_dif)
+#print(sym_diff_as_list)
+
+"""Ranked neutral reagent list"""
+ranked_list = intersection_as_list + sym_diff_as_list
+print(ranked_list)
 
 
 
