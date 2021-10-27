b9b56452c7c6726ecb162e50073bc4b5ec532a19
gwijewar <gwijewar@purdue.edu>
2021-10-27 15:54:01 -0400

    expert_funcs_updated

--- functionality_prediction_model/functionality_prediction_script.py
+++ functionality_prediction_model/functionality_prediction_script.py
@@ -7,12 +7,15 @@ Created on Mon Oct 18 11:46:16 2021
 """
 
 import pandas as pd
+import itertools
 
 df = pd.read_csv("rt30ms.csv") # reading the csv file
 analyte_mz = 101.1 # protonated analyte m/z to the decimal place as reported in the reading csv file
 relative_cutoff = 2 # relative cutoff using . default has been set to 2
-elem_comp = ""
-rdbe = 2
+elem_comp = "C12H11O1S1"
+rdbe = 9
+
+
 
 expert_based_dict = {73.04: ['epoxide','sulfone','sulfoxide', 'amide', 'alcohol', 'aldehyde', 'ether', 'ketone', 'ester', 'carboxylic acid'], #TMB adduct-MeOH
                      59.03: ['epoxide', 'sulfone'], #TMB adduct-Me2O
@@ -20,8 +23,31 @@ expert_based_dict = {73.04: ['epoxide','sulfone','sulfoxide', 'amide', 'alcohol'
                      72.06: ['sulfoxide', 'N,N-disubstituted_hydroxylamine', 'aromatic_tertiary_N-oxide'], #MOP adduct
                      52.04: ['N-oxide_with_nearby_COOH/OH/NH2', 'sulfoxide_with_nearby_COOH/OH/NH2'], #TDMAB adduct-2DMA
                      98.10: ['N-oxide', 'sulfoxide', 'urea', 'pyridine', 'imine']} #TDMAB adduct-DMA
-ml_based_dict = {}
+ml_based_dict = {73.04: 'tmb_frags/1153_15.svg', #TMB adduct-MeOH
+                     59.03: [], #TMB adduct-Me2O
+                     105.07: [], #TMB adduct
+                     72.06: [], #MOP adduct
+                     52.04: [], #TDMAB adduct-2DMA
+                     98.10: []}
 
+funcs_n_elemental_comps = {'sulfoxide': ['S','O'],
+                           'N,N-disubstituted_hydroxylamine': ['N','O'], 
+                           'aromatic_tertiary_N-oxide': ['N','O'],
+                           'epoxide':['O'],
+                           'sulfone':['S','O'],
+                           'amide':['N','O'], 
+                           'alcohol':['O'], 
+                           'aldehyde':['O'], 
+                           'ether':['O'], 
+                           'ketone':['O'], 
+                           'ester':['O'], 
+                           'carboxylic acid':['O'],
+                           'N-oxide_with_nearby_COOH/OH/NH2':['N','O'], 
+                           'sulfoxide_with_nearby_COOH/OH/NH2':['S','O'],
+                           'N-oxide':['N','O'], 
+                           'urea':['N','O'], 
+                           'pyridine':['N'], 
+                           'imine':['N']}
 
 def split(word):
     return list(word)
@@ -59,7 +85,7 @@ def br_calculation(df, analyte, cutoff):
     df_selected = df.loc[df['Relative'] >= relative_cutoff] # select the peaks above a certain relative intensity cutoff
     df_dropped = df_selected.loc[df['m/z'] != analyte] # protonated analyte dropped!
     sum_relative = sum(df_dropped['Relative'])
-    df_dropped['Branching_ratios']= (df_dropped['Relative']/sum_relative)*100 #BR calculation
+    df_dropped['Branching_ratios'] = (df_dropped['Relative']/sum_relative)*100 #BR calculation
     return df_dropped
 
 def mass_difference(df, analyte):
@@ -90,8 +116,55 @@ result_df = br_calculation(df, analyte_mz, relative_cutoff)
 result_df2 = mass_difference(result_df, analyte_mz)
 
 expert_based_funcs = expert_based(result_df2, 'None', expert_based_dict)
+expert_based_funcs = list(itertools.chain(*expert_based_funcs))
+elements_list_of_list = find_elements(elem_comp)
 
-print(*expert_based_funcs)
+#print(expert_based_funcs)
+#print(funcs_n_elemental_comps)
+#print(elements_list_of_list)
+
+
+def func_sieve_expert(elements_lst_of_lst, predicted_func_list, func_and_elements_dict):
+    lst_of_lst = elements_lst_of_lst
+    elements = lst_of_lst[0]
+    numbers = lst_of_lst[1]
+    prob_funcs = []
+    for i in predicted_func_list:
+        if(set(func_and_elements_dict[i]).issubset(set(elements))) and numbers[elements.index('O')] > 0 or  (set(elements).issubset(set(func_and_elements_dict[i]))) and numbers[elements.index('O')] > 0:
+            prob_funcs.append(i)
+        else:
+            continue
+        
+    return prob_funcs
+
+print(func_sieve_expert(elements_list_of_list,expert_based_funcs,funcs_n_elemental_comps))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
